ffe41e0313fad16df10fb63a04aea2829db338ef
dheerajdataclaps <dheerajdataclaps@gmail.com>
2025-06-22 17:38:04 +0530

    updated task model for checking migrations.

--- api/models.py
+++ api/models.py
@@ -6,6 +6,7 @@ class Task(models.Model):
     completed = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    due_date = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
         return self.title
